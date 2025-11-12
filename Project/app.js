69660f4dd242de789c63cc81467a30bfb6519630
Paoo <thienhanaylai@gmail.com>
2025-11-12 13:43:24 +0700

    Cập nhật giao diện admin

--- Project/app.js
+++ Project/app.js
@@ -55,7 +55,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error", { err: err.status });
+  res.render("error", { errStatus: err.status });
 });
 
 module.exports = app;
--- Project/routes/admin.js
+++ Project/routes/admin.js
@@ -18,7 +18,28 @@ router.get("/product", function (req, res, next) {
   res.render("admin/product");
 });
 
+router.get("/product/add", function (req, res, next) {
+  res.render("admin/product/addProduct");
+});
+
+//edit sẽ truyền thêm id sản phẩm
+router.get("/product/edit", function (req, res, next) {
+  res.render("admin/product/editProduct");
+});
+
 router.get("/category", function (req, res, next) {
   res.render("admin/category");
+});
+
+router.get("/category/add", function (req, res, next) {
+  res.render("admin/category/addCategory");
+});
+
+router.get("/category/edit", function (req, res, next) {
+  res.render("admin/category/editCategory");
+});
+
+router.get("/orders", function (req, res, next) {
+  res.render("admin/orders");
 });
 module.exports = router;
